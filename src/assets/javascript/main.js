708e0b31011415010c002e67dea75ebe45b59cc9
Nick Brunner <njbrunner@gmail.com>
2022-11-28 13:15:18 -0700

    Added method to post to cloud function

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -2,7 +2,8 @@ function submitRSVP() {
   let data = {
     name: document.getElementById("nameInput").value,
     phone: document.getElementById("phoneInput").value,
-    attending: document.getElementById("attendInput").value,
+    attending: document.querySelector('input[name="rsvp-selection"]:checked')
+      .value,
   };
 
   axios({
@@ -12,8 +13,20 @@ function submitRSVP() {
   })
     .then((response) => {
       console.log(response);
+      let toastElement = document.getElementById("toast");
+      toastElement.classList.remove("text-bg-danger");
+      toastElement.classList.add("text-bg-success");
+      toastElement.querySelector(".toast-body").innerHTML = response.data;
+      const toast = new bootstrap.Toast(toastElement);
+      toast.show();
     })
     .catch((error) => {
       console.log(error);
+      let toastElement = document.getElementById("toast");
+      toastElement.classList.remove("text-bg-success");
+      toastElement.classList.add("text-bg-danger");
+      toastElement.querySelector(".toast-body").innerHTML = error.response.data;
+      const toast = new bootstrap.Toast(toastElement);
+      toast.show();
     });
 }
